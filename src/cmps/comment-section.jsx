f1120f76e8e24d9fce70ffc45766e9dc9daf542e
yonatan104 <yonatangeller@gmail.com>
2022-06-15 14:49:23 +0300

    sockets add functinality is ready

--- src/cmps/comment-section.jsx
+++ src/cmps/comment-section.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import { CommentList } from './comment-list'
 import { commentService } from '../services/comment.service'
+import { socketService , SOCKET_EVENT_COMMENT_ADDED, SOCKET_EVENT_COMMENT_ABOUT_YOU} from '../services/socket.service'
 
 export const CommentSection = () => {
 
@@ -8,6 +9,24 @@ export const CommentSection = () => {
 
     useEffect(() => {
         loadComments()
+    }, [])
+
+
+    useEffect(() => {
+        socketService.off(SOCKET_EVENT_COMMENT_ADDED, loadComments)
+        socketService.on(SOCKET_EVENT_COMMENT_ADDED, loadComments)
+        socketService.off(SOCKET_EVENT_COMMENT_ABOUT_YOU, loadComments)
+        socketService.on(SOCKET_EVENT_COMMENT_ABOUT_YOU, loadComments)
+        // if (stationId) {
+        //     socketService.emit(SOCKET_EMIT_ENTERED_STATION, stationId)
+        // }   
+
+        loadComments()
+
+        return () => {
+            socketService.off(SOCKET_EVENT_COMMENT_ADDED, loadComments)
+            socketService.off(SOCKET_EVENT_COMMENT_ABOUT_YOU, loadComments)
+        }
     }, [])
 
     const loadComments = async () => {
--- src/services/socket.service.js
+++ src/services/socket.service.js
@@ -1,12 +1,8 @@
 import io from 'socket.io-client'
 
-// export const SOCKET_EVENT_ADD_MSG = 'chat-add-msg'
-// export const SOCKET_EMIT_SEND_MSG = 'chat-send-msg'
-// export const SOCKET_EMIT_SET_TOPIC = 'chat-set-topic'
-// export const SOCKET_EMIT_USER_WATCH = 'user-watch'
-// export const SOCKET_EVENT_USER_UPDATED = 'user-updated'
-// export const SOCKET_EVENT_REVIEW_ADDED = 'review-added'
-// export const SOCKET_EVENT_REVIEW_ABOUT_YOU = 'review-about-you'
+
+export const SOCKET_EVENT_COMMENT_ADDED = 'comment-added'
+export const SOCKET_EVENT_COMMENT_ABOUT_YOU = 'comment-about-you'
 
 const SOCKET_EMIT_LOGIN = 'set-user-socket'
 const SOCKET_EMIT_LOGOUT = 'unset-user-socket'
