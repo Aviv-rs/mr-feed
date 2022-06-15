aa42134de411342201e64aa5e486c8e616143daa
Aviv <aviv80010@gmail.com>
2022-06-15 16:00:29 +0300

    changed grid layout of comment preview

--- src/cmps/comment-preview.jsx
+++ src/cmps/comment-preview.jsx
@@ -1,16 +1,18 @@
 import defaultAvatar from '../assets/imgs/default-avatar.png'
 
 export const CommentPreview = ({ comment }) => {
-    return <div className="comment-preview">
+    return <article className="comment-preview">
         <div className="comment-avatar-container">
             <img className="comment-avatar" src={comment.avatar || defaultAvatar} alt="" />
         </div>
-        <h3 className="comment-email">
-            {comment.email}
-        </h3>
+        <div className="comment-details flex column">
+            <h3 className="comment-email">
+                {comment.email}
+            </h3>
 
-        <div className="comment-txt">
-            {comment.txt}
+            <div className="comment-txt">
+                {comment.txt}
+            </div>
         </div>
-    </div>
+    </article>
 }
